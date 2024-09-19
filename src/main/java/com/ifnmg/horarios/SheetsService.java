217ae6d1d4a54ad72853713ac6c594591c7973f4
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2024-09-19 01:15:01 -0300

    Teste

--- src/main/java/com/ifnmg/horarios/SheetsService.java
+++ src/main/java/com/ifnmg/horarios/SheetsService.java
@@ -7,19 +7,16 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import org.springframework.stereotype.Service;
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
 @Service
 public class SheetsService {
-    private static final Dotenv dotenv = Dotenv.load();
     private static final String APPLICATION_NAME = "Horários do IFNMG Salinas";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String API_KEY = dotenv.get("API_KEY");
-    private final String spreadsheetId = dotenv.get("SPREADSHEET_ID");
+    private static final String API_KEY = System.getenv("API_KEY");
+    private static final String SPREADSHEET_ID = System.getenv("SPREADSHEET_ID");
     private final Sheets sheetsService;
     public SheetsService() throws GeneralSecurityException, IOException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -31,7 +28,7 @@ public class SheetsService {
     public List<List<Object>> getSheetValues(String range) throws IOException {
         // Inclua a API key na chamada
         ValueRange response = sheetsService.spreadsheets().values()
-                .get(spreadsheetId, range)
+                .get(SPREADSHEET_ID, range)
                 .setKey(API_KEY)  // Adiciona a chave de API
                 .execute();
         return response.getValues();
